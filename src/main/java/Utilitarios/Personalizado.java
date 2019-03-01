8d250b86c6215a2fce3f23a8868833b19f73a6fb
Victor <vicmacondo@hotmail.com>
2019-03-01 11:18:52 -0500

    vuelvo a subir clases pero esta vez organizadas.

---
+++ src/main/java/Utilitarios/Personalizado.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Utilitarios;
+
+
+
+/**
+ *
+ * @author fetec
+ */
+public class Personalizado extends Vehiculo{
+    
+    private boolean aleron;
+    private boolean nitro;
+
+    public Personalizado(boolean aleron, boolean nitro, StringBuffer marca, StringBuffer modelo, StringBuffer color, int cantidad) {
+        super(marca, modelo, color, cantidad);
+        this.aleron = aleron;
+        this.nitro = nitro;
+    }
+    
+    @Override
+    public void verDetalle() {
+        
+    }
+
+    public boolean isAleron() {
+        return aleron;
+    }
+
+    public void setAleron(boolean aleron) {
+        this.aleron = aleron;
+    }
+
+    public boolean isNitro() {
+        return nitro;
+    }
+
+    public void setNitro(boolean nitro) {
+        this.nitro = nitro;
+    }
+    
+    
+    
+}
